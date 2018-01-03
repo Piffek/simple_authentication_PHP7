0d4e0afe75cdd767cb65083cfcf8cbbc636c2dfa
Piffek <patrykpiwko123412@gmail.com>
2018-01-03 21:21:35 +0100

    use method

--- index.php
+++ index.php
@@ -1,17 +1,35 @@
 <?php
 
 session_start();
-if (! isset($_SESSION['token'])) {
-    $_SESSION['token'] = bin2hex(random_bytes(32));
-    $token = $_SESSION['token'];
-} else {
-    $token = $_SESSION['token'];
-}
 
-if (isset($_POST['submit']) && hash_equals($_SESSION['token'], $_POST['token'])) {
+$token = function (string $lock = null) {
+    if (! isset($_SESSION['token'])) {
+        $_SESSION['token'] = bin2hex(random_bytes(32));
+    }
+
+    if (! isset($_SESSION['second_token'])) {
+        $_SESSION['second_token'] = bin2hex(random_bytes(32));
+    }
+
+    if (is_null($lock)) {
+        $_SESSION['rightToken'] = $_SESSION['token'];
+    }
+
+    $_SESSION['rightToken'] = hash_hmac('sha256', $lock, $_SESSION['second_token']);
+
+    return $_SESSION['rightToken'];
+};
+
+if (isset($_POST['token']) && hash_equals($_POST['token'], $_SESSION['rightToken'])) {
     echo $_POST['name']."\n";
-    echo $_SESSION['token'];
+    echo $_SESSION['rightToken'];
 }
 
 ?>
+
+<form method="POST" action="/">
+    <input type="hidden" name="token" value="<?php echo $token() ?>">
+    <input type="text" name="name">
+    <input type="submit" name="submit" value="save">
+</form>
 
